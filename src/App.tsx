6f4c10d3a072b983713988d045cf291bacf4de7b
Rob Butcher <robert.butcher@skyral.io>
2026-08-26 17:23:57 +0100

    Updating for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { LensApp } from "@adk/lens-react";
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <LensApp />
+  return <LensApp />;
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import lensConfigPlugin from "@adk/lens-react/vite-plugin";
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { protomapsAssetsPlugin } from "@adk/vite-plugin-protomaps-assets";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), lensConfigPlugin()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    lensConfigPlugin(),
+    protomapsAssetsPlugin({ flavors: ["dark"] }),
+  ],
+  resolve: {
+    dedupe: ["@emotion/react", "@emotion/styled"],
+  },
+});
